f125de6e96f0b9d3830d6870d964e03881d598e2
serge <lemouton@ircam.Fr>
2021-02-26 20:05:06 +0100

    added profiles viewset and permissions

---
+++ profiles_api/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+class UpdateOwnProfile(permissions.BasePermission):
+	"""allows user to edit theri own profiles"""
+	def has_object_permission(self,request,view,obj):
+		"""check if user is tryind to edit their own profile"""
+		if request.method on permissions.SAFE_METHODS:
+			return True
+		return obj.id == request.user.id
+		
