d516e8170aefdd132fdea03a6c02edf43b9a8952
serge <lemouton@ircam.Fr>
2021-02-26 20:13:40 +0100

    added searching to API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -4,7 +4,7 @@ class UpdateOwnProfile(permissions.BasePermission):
 	"""allows user to edit theri own profiles"""
 	def has_object_permission(self,request,view,obj):
 		"""check if user is tryind to edit their own profile"""
-		if request.method on permissions.SAFE_METHODS:
+		if request.method in permissions.SAFE_METHODS:
 			return True
 		return obj.id == request.user.id
-		
+
