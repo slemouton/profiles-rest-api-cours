ebbf37ce407e719a28deadd55919f5a6392252b9
serge <lemouton@ircam.Fr>
2021-02-27 12:23:21 +0100

    added user profile Feed API

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -8,3 +8,11 @@ class UpdateOwnProfile(permissions.BasePermission):
 			return True
 		return obj.id == request.user.id
 
+class UpdateOwnStatus(permissions.BasePermission):
+	"""allow user to updtate their own status"""
+	def has_object_permission(self,request,view,obj):
+		if request.method in permissions.SAFE_METHODS:
+			return True
+		return obj.user_profile.id == request.user.id
+
+
